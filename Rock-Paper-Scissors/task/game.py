478494d60736c81b6bcdb2325b8f00dc16115a77
RendelFlegg <62382457+RendelFlegg@users.noreply.github.com>
2022-01-30 17:46:28 +0500

    Completed stage 1

--- Rock-Paper-Scissors/task/game.py
+++ Rock-Paper-Scissors/task/game.py
@@ -1,1 +1,6 @@
-# Write your code here
+dictionary = {'rock': {'win': 'scissors', 'lose': 'paper'},
+              'paper': {'win': 'rock', 'lose': 'scissors'},
+              'scissors': {'win': 'paper', 'lose': 'rock'}}
+
+user_input = input()
+print('Sorry, but the computer chose', dictionary[user_input]['lose'])
