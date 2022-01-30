7d5cd7c5bdc99d1ab29801ec60c7a0ac67c1375b
RendelFlegg <62382457+RendelFlegg@users.noreply.github.com>
2022-01-30 17:54:35 +0500

    Completed stage 2: Implemented computer move

--- Rock-Paper-Scissors/task/game.py
+++ Rock-Paper-Scissors/task/game.py
@@ -1,6 +1,16 @@
+import random
+
 dictionary = {'rock': {'win': 'scissors', 'lose': 'paper'},
               'paper': {'win': 'rock', 'lose': 'scissors'},
               'scissors': {'win': 'paper', 'lose': 'rock'}}
 
-user_input = input()
-print('Sorry, but the computer chose', dictionary[user_input]['lose'])
+user_move = input()
+computer_move = random.choice(list(dictionary.keys()))
+
+if user_move == computer_move:
+    print(f'There is a draw ({user_move})')
+else:
+    if dictionary[user_move]['lose'] == computer_move:
+        print('Sorry, but the computer chose', computer_move)
+    else:
+        print(f'Well done. The computer chose {computer_move} and failed')
