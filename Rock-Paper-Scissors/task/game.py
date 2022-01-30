7118c3b819e74c9cb79160f4e9a54dbab4cea45c
RendelFlegg <62382457+RendelFlegg@users.noreply.github.com>
2022-01-30 18:02:26 +0500

    Completed stage 3: Implemented endless game feature

--- Rock-Paper-Scissors/task/game.py
+++ Rock-Paper-Scissors/task/game.py
@@ -4,13 +4,20 @@ dictionary = {'rock': {'win': 'scissors', 'lose': 'paper'},
               'paper': {'win': 'rock', 'lose': 'scissors'},
               'scissors': {'win': 'paper', 'lose': 'rock'}}
 
-user_move = input()
-computer_move = random.choice(list(dictionary.keys()))
 
-if user_move == computer_move:
-    print(f'There is a draw ({user_move})')
-else:
-    if dictionary[user_move]['lose'] == computer_move:
-        print('Sorry, but the computer chose', computer_move)
+while True:
+    user_move = input()
+    if user_move not in list(dictionary.keys()) and user_move != '!exit':
+        print('Invalid input')
+        continue
+    if user_move == '!exit':
+        print('Bye!')
+        break
+    computer_move = random.choice(list(dictionary.keys()))
+    if user_move == computer_move:
+        print(f'There is a draw ({user_move})')
     else:
-        print(f'Well done. The computer chose {computer_move} and failed')
+        if dictionary[user_move]['lose'] == computer_move:
+            print('Sorry, but the computer chose', computer_move)
+        else:
+            print(f'Well done. The computer chose {computer_move} and failed')
